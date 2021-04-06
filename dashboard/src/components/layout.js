70c45ba2fd86b07ad356b56ad8050bb74ad199cb
Niamh McCooey <niamhmccooey@gmail.com>
2021-04-06 17:51:35 +0100

    chore: add identity widget

--- dashboard/src/components/layout.js
+++ dashboard/src/components/layout.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { IdentityContextProvider } from 'react-netlify-identity-widget';
 import './layout.css';
 
 const Layout = ({ children }) => (
-	<>
+	// Point the context provider to a working netlify site that has identity enabled,
+	// so we'll have to deploy this site in order to get the auth working
+	<IdentityContextProvider url='https://jamstack-intro-auth.netlify.com'>
 		<header>
 			<Link to='/'>JAMStack App</Link>
 		</header>
 		<main>{children}</main>
-	</>
+	</IdentityContextProvider>
 );
 
 export default Layout;
