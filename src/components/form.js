0a9951bd18c519a7184f6c2bf65ba3371af0f3c1
Niamh McCooey <niamhmccooey@gmail.com>
2021-04-06 12:58:02 +0100

    chore: - handle UI based on state - create setStatus function that dispatches an updateStatus type - mimic success using setTimeout & add reset btn to success page

--- src/components/form.js
+++ src/components/form.js
@@ -43,17 +43,60 @@ const StyledForm = styled.form`
 	}
 `;
 
+const SentMsg = styled.p`
+	border-radius: right 0.25rem;
+	display: block;
+	margin: 0.75rem auto 0;
+	max-width: width 500px;
+	text-align: center;
+`;
+
+const SuccessWrap = styled.div`
+	button {
+		display: block;
+		margin: 1rem auto;
+	}
+`;
+
+const PendingForm = styled(StyledForm)`
+	&::before {
+		background: white;
+		border-radius: 0.25rem;
+		content: '';
+		height: 100vh;
+		width: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		opacity: 0.85;
+	}
+
+	&::after {
+		content: 'Sending...';
+		margin: 0 auto;
+		color: black;
+		display: block;
+		height: 6rem;
+		width: 6rem;
+		position: relative;
+	}
+`;
+
 const INITIAL_STATE = {
 	name: '',
 	email: '',
 	subject: '',
 	body: '',
+	status: 'IDLE',
 };
 
 const reducer = (state, action) => {
 	switch (action.type) {
 		case 'updateFieldValue':
 			return { ...state, [action.field]: action.value };
+		case 'updateStatus':
+			return { ...state, status: action.status };
+		case 'reset':
 		default:
 			return INITIAL_STATE;
 	}
@@ -61,6 +104,9 @@ const reducer = (state, action) => {
 
 const Form = () => {
 	const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+	// this'll take a status field value & dispatch a type of 'updateStatus' & send the new status as its value
+	const setStatus = (status) => dispatch({ type: 'updateStatus', status });
 
 	// currying: instead of creating a function with 2 args, we call it with the 1st arg
 	// and return another function that accepts the 2nd arg
@@ -74,48 +120,71 @@ const Form = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		setStatus('PENDING');
+		setTimeout(() => setStatus('SUCCESS'), 1000);
 	};
 	// TODO: send the message
-	console.log(state);
+	// console.log(state);
+
+	if (state.status === 'SUCCESS') {
+		return (
+			<SuccessWrap>
+				<SentMsg>Message sent!</SentMsg>
+				<button type='reset' onClick={() => dispatch({ type: 'reset' })}>
+					Reset
+				</button>
+			</SuccessWrap>
+		);
+	}
+
 	return (
-		<StyledForm onSubmit={handleSubmit}>
-			<label>
-				Name
-				<input
-					type='text'
-					name='name'
-					value={state.name}
-					onChange={updateFieldValue('name')}
-				/>
-			</label>
-			<label>
-				Email
-				<input
-					type='email'
-					name='email'
-					value={state.email}
-					onChange={updateFieldValue('email')}
-				/>
-			</label>
-			<label>
-				Subject
-				<input
-					type='text'
-					name='subject'
-					value={state.subject}
-					onChange={updateFieldValue('subject')}
-				/>
-			</label>
-			<label>
-				Body
-				<textarea
-					name='body'
-					value={state.body}
-					onChange={updateFieldValue('body')}
-				/>
-			</label>
-			<button>Send</button>
-		</StyledForm>
+		<>
+			{state.status === 'ERROR' && (
+				<SentMsg>Something went wrong. Please try again.</SentMsg>
+			)}
+			{state.status === 'PENDING' ? (
+				<PendingForm />
+			) : (
+				<StyledForm onSubmit={handleSubmit}>
+					<label>
+						Name
+						<input
+							type='text'
+							name='name'
+							value={state.name}
+							onChange={updateFieldValue('name')}
+						/>
+					</label>
+					<label>
+						Email
+						<input
+							type='email'
+							name='email'
+							value={state.email}
+							onChange={updateFieldValue('email')}
+						/>
+					</label>
+					<label>
+						Subject
+						<input
+							type='text'
+							name='subject'
+							value={state.subject}
+							onChange={updateFieldValue('subject')}
+						/>
+					</label>
+					<label>
+						Body
+						<textarea
+							name='body'
+							value={state.body}
+							onChange={updateFieldValue('body')}
+						/>
+					</label>
+					<button>Send</button>
+				</StyledForm>
+			)}
+		</>
 	);
 };
 
